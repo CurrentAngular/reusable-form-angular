6b365cb0965bcf27b529f46d74a7052baa0f8026
Valery Semenenko <gearmobile@gmail.com>
2026-01-02 18:49:10 +0300

    chore: initial setup project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,8 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideEventPlugins()],
 };
